a323e1627b66149f2df84aa7b7a2db4d48053e72
mingrui.wang2 <mingrui.wang2@bytedance.com>
2026-08-27 10:47:40 +0000

    change order

--- flash_qla/utils/env.py
+++ flash_qla/utils/env.py
@@ -7,6 +7,6 @@ from packaging.version import Version
 
 TILELANG_VERSION = tilelang.__version__
 
-TILELANG_0_1_11 = Version(TILELANG_VERSION) == Version("0.1.11")
 TILELANG_0_1_9 = Version(TILELANG_VERSION) == Version("0.1.9")
+TILELANG_0_1_11 = Version(TILELANG_VERSION) == Version("0.1.11")
 TILELANG_0_1_12 = Version(TILELANG_VERSION) == Version("0.1.12")
